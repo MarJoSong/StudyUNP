51012923bf638692b6f3b3ab02ef8ba12258da1f
huahua <769200142@qq.com>
2021-08-13 20:28:44 +0800

    1.大小端传输测试 2.四次挥手分节

---
+++ Chapter5/sum.h
@@ -0,0 +1,17 @@
+//
+// Created by Mars on 2021/8/13.
+//
+
+#ifndef UNP_CHAPTER5_SUM_H_
+#define UNP_CHAPTER5_SUM_H_
+
+struct args {
+  long arg1;
+  long arg2;
+};
+
+struct result {
+  long sum;
+};
+
+#endif //UNP_CHAPTER5_SUM_H_
--- Chapter5/tcpcliv01.c
+++ Chapter5/tcpcliv01.c
@@ -2,13 +2,18 @@
 // Created by mars on 2021/8/11.
 //
 #include "unp.h"
+#include "sum.h"
 
-void my_str_cli(int sockfd) {
+void my_cli_deal(int sockfd) {
+#if defined(ECHO_TEST)
   char sendline[MAXLINE], recvline[MAXLINE];
+  bzero(sendline, MAXLINE);
 
   while (Fgets(sendline, MAXLINE, stdin) != NULL) {
-#if 0
+#if 1
     Write(sockfd, sendline, strlen(sendline));
+    //读入数据之前需要清空缓存区
+    bzero(sendline, MAXLINE);
 #else
     //在客户端阻塞于输入时杀死服务端子进程
     //客户端第一次写收到RST
@@ -23,6 +28,22 @@ void my_str_cli(int sockfd) {
 
     Fputs(recvline, stderr);
   }
+#elif defined(ENDIAN_TEST)
+  char sendline[MAXLINE], recvline[MAXLINE];
+  struct args args;
+  struct result result;
+  while (Fgets(sendline, MAXLINE, stdin) != NULL) {
+    if (sscanf(sendline, "%ld %ld", &args.arg1, &args.arg2) != 2) {
+      fprintf(stderr, "invalid input:%s\n", sendline);
+      continue;
+    }
+    Write(sockfd, &args, sizeof(args));
+    if (Read(sockfd, &result, sizeof(result)) ==0) {
+      err_quit("str_cli: server terminated permaturely");
+    }
+    fprintf(stderr, "sum:%ld\n", result.sum);
+  }
+#endif
 }
 
 int main(int argc, char **argv) {
@@ -39,7 +60,7 @@ int main(int argc, char **argv) {
 
   Connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-  my_str_cli(sockfd);
+  my_cli_deal(sockfd);
 
   exit(0);
 }
